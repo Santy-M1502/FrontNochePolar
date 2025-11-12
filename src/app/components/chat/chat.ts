1d7b77fbb85ac73ed49c59d5dcc0c66e2e5fec95
Santy_M <santy15021502@gmail.com>
2025-11-11 23:23:39 -0300

    minicambio

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, signal, computed, AfterViewChecked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SocketService } from '../../services/socket.service';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { ChatHttpService } from '../../services/chat.service';
@@ -18,38 +17,27 @@ export class Chat implements AfterViewChecked {
   @ViewChild('chatBox') chatBox!: ElementRef<HTMLDivElement>;
 
   constructor(
-    private socketService: SocketService,
     private authService: AuthService,
     private userService: UserService,
     private chatHttp: ChatHttpService,
   ) {}
 
   chatOpen = signal(false);
-
   amigos = signal<{ id: string; nombre: string }[]>([]);
-
   destinatarioId = signal<string | null>(null);
-
   chats = signal<{ me: boolean; text: string }[]>([]);
   newMessage = signal('');
 
   tieneChatAbierto = computed(() => this.destinatarioId() !== null);
 
   ngOnInit() {
-    const token = this.authService.getToken() || '';
-    this.socketService.connect(token);
-
     this.userService.getFriends().subscribe((res: any) => {
-      this.amigos.set(res.map((u: any) => ({
-        id: u._id,
-        nombre: u.username
-      })));
-    });
-
-    this.socketService.onMessage((data) => {
-      if (data.from === this.destinatarioId()) {
-        this.chats.update(curr => [...curr, { me: false, text: data.msg }]);
-      }
+      this.amigos.set(
+        res.map((u: any) => ({
+          id: u._id,
+          nombre: u.username,
+        }))
+      );
     });
   }
 
@@ -74,8 +62,6 @@ export class Chat implements AfterViewChecked {
   sendMessage() {
     const msg = this.newMessage().trim();
     if (!msg || !this.destinatarioId()) return;
-
-    this.socketService.sendMessage(this.destinatarioId()!, msg);
 
     this.chats.update(curr => [...curr, { me: true, text: msg }]);
 
